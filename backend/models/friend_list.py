14ac3d059551d8324347f90308edfdd70620172f
TRBF <trbf.contact@gmail.com>
2023-09-12 22:17:03 +0300

    Friend List service somewhat done, renamed router.py to location_controller.py WARNING: UNTESTED

--- backend/models/friend_list.py
+++ backend/models/friend_list.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Mapped
 
 
 class FriendList(db.Model):
-    __tablename__ = "user_to_friends_list"
+    __tablename__ = "user_to_friend_list"
 
     username: Mapped[str] = db.mapped_column(db.ForeignKey(User.username), primary_key=True)
     friend_list: Mapped[Optional[str]] = db.mapped_column() # We store the list as a string of usernames that will be joined by the \0 character 
--- backend/services/friend_list_service.py
+++ backend/services/friend_list_service.py
@@ -1,7 +1,25 @@
 from backend.models.friend_list import FriendList
-from backend.repository.control import ControlScheme 
+from backend.models.user import User
+from backend.repository.friend_list_repository import FriendListRepository
 
-def service_add_friend(user, friend_id):
-   pass 
+def service_add_friend(user: User, friend: User):
+    user_friend_list: Friendlist = FriendListRepository().get_with_key(user.username)
+    friend_friend_list: Friendlist = FriendListRepository().get_with_key(friend.username)
+
+    if (user.friend_list.friend_list.find(friend.username) == -1):
+        FriendListRepository().update(username, user.username, user_friend_list = user_friend_list + friend.username)
+        FriendListRepository().update(username, friend.username, friend_friend_list = friend_friend_list + user.username)
+
+def service_remove_friend(user: User, friend: User):
+    user_friend_list: Friendlist = FriendListRepository().get_with_key(user.username)
+    friend_friend_list: Friendlist = FriendListRepository().get_with_key(friend.username)
+
+    if (user.friend_list.friend_list.find(friend.username) != -1):
+        FriendListRepository().update(username, user.username, user_friend_list = user_friend_list.replace(friend.username, ''))
+        FriendListRepository().update(username, friend.username, friend_friend_list = friend_friend_list.replace(user.username, ''))
+
+def service_get_friends(user: User, friend: User):
+    user_friend_list:FriendList = FriendListRepository().get_with_key(user.username)
+    return user_friend_list.friend_list
 
 
