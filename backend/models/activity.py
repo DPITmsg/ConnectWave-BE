cce9ed591c6076aae5d065432e86d40b1a4e5df8
TRBF <trbf.contact@gmail.com>
2023-11-07 20:29:34 +0200

    Minor fixes done. Project runs and responds to get requests properly

--- backend/models/activity.py
+++ backend/models/activity.py
@@ -3,7 +3,7 @@ from typing import Optional, List
 from config import db
 from models.location import Location
 from sqlalchemy.orm import Mapped
-from backend.models.user import User
+from models.user import User
 
 
 class Activity(db.Model):
@@ -24,8 +24,8 @@ class Activity(db.Model):
     address: Mapped[Optional[str]] = db.mapped_column(default="")
     location_id: Mapped[Optional[int]] = db.mapped_column(db.ForeignKey(Location.id))
 
-    number_of_participants: Mapped[Optional[int]] = db.mapped_column(default=0)
+    participants: Mapped[Optional[int]] = db.mapped_column(default=0)
     max_participants: Mapped[Optional[int]] = db.mapped_column(default=0)
 
     def __repr__(self):
-        return f"({self.id}) {self.name}: {self.description} | {self.number_of_participants} participants"
+        return f"({self.id}) {self.name}: {self.description} | {self.participants} participants"
